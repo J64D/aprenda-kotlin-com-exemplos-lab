e9d3d5f9bff0350347c140e23356717a8f35bded
J64D <88570215+J64D@users.noreply.github.com>
2023-09-29 21:27:50 -0300

    Update desafio.kt

--- desafio.kt
+++ desafio.kt
@@ -1,21 +1,40 @@
-// [Template no Kotlin Playground](https://pl.kotl.in/WcteahpyN)
-
 enum class Nivel { BASICO, INTERMEDIARIO, AVANCADO }
 
-class Usuario
+class Usuario(val nome: String, val nivel: Nivel)
 
 data class ConteudoEducacional(var nome: String, val duracao: Int = 60)
 
 data class Formacao(val nome: String, var conteudos: List<ConteudoEducacional>) {
-
     val inscritos = mutableListOf<Usuario>()
-    
+
     fun matricular(usuario: Usuario) {
-        TODO("Utilize o parâmetro $usuario para simular uma matrícula (usar a lista de $inscritos).")
+        inscritos.add(usuario)
+        println("${usuario.nome} foi matriculado na formação $nome.")
     }
 }
 
 fun main() {
-    TODO("Analise as classes modeladas para este domínio de aplicação e pense em formas de evoluí-las.")
-    TODO("Simule alguns cenários de teste. Para isso, crie alguns objetos usando as classes em questão.")
+    // Criando alguns conteúdos educacionais
+    val htmlBasico = ConteudoEducacional("HTML Básico")
+    val cssBasico = ConteudoEducacional("CSS Básico")
+    val kotlinBasico = ConteudoEducacional("Kotlin Básico")
+
+    // Criando uma formação
+    val formacaoWeb = Formacao("Desenvolvimento Web", listOf(htmlBasico, cssBasico, kotlinBasico))
+
+    // Criando alguns usuários
+    val usuario1 = Usuario("Alice", Nivel.BASICO)
+    val usuario2 = Usuario("Bob", Nivel.INTERMEDIARIO)
+    val usuario3 = Usuario("Charlie", Nivel.AVANCADO)
+
+    // Matriculando usuários na formação
+    formacaoWeb.matricular(usuario1)
+    formacaoWeb.matricular(usuario2)
+    formacaoWeb.matricular(usuario3)
+
+    // Exibindo usuários inscritos na formação
+    println("Usuários inscritos na formação ${formacaoWeb.nome}:")
+    for (usuario in formacaoWeb.inscritos) {
+        println("- ${usuario.nome} (Nível: ${usuario.nivel})")
+    }
 }
